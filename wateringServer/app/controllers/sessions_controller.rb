4959993ee97a9f215e4c6578a2f517726a8ffec8
David Armbrust <david.armbrust@gmail.com>
2016-10-10 13:07:27 -0500

    Add strong params and user authenticate

--- wateringServer/app/controllers/sessions_controller.rb
+++ wateringServer/app/controllers/sessions_controller.rb
@@ -4,8 +4,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.
+      find_by(email: user_params[:email]).
+      try(:authenticate, user_params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:session).permit(:email, :password)
   end
 end
