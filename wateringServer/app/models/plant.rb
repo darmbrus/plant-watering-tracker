171ecfb0b208fdb061bf6aca044751fb64a465b9
David Armbrust <david.armbrust@gmail.com>
2016-10-11 11:34:27 -0500

    Add needs water logic and display

--- wateringServer/app/models/plant.rb
+++ wateringServer/app/models/plant.rb
@@ -2,10 +2,12 @@ class Plant < ApplicationRecord
   has_many :waterings
 
   def latest_watering
+    # TODO handle no waterings
     self.waterings.order(date: :desc).first
   end
 
   def needs_water?
-    
+    # TODO handle no waterings
+    Date.today() - self.latest_watering.date > self.days_per_watering
   end
 end
