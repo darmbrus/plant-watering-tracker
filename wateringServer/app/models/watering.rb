ee2a7cc69dd394d951e541cf5b251b2519f1ea93
David Armbrust <david.armbrust@gmail.com>
2016-10-11 14:28:00 -0500

    Add validations to watering

--- wateringServer/app/models/watering.rb
+++ wateringServer/app/models/watering.rb
@@ -1,4 +1,7 @@
 class Watering < ApplicationRecord
   belongs_to :user
   belongs_to :plant
+
+  validates :date, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
