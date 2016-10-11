f8c7c767c26e09ef482ecb3a2e0bbf5110696eda
David Armbrust <david.armbrust@gmail.com>
2016-10-11 09:17:22 -0500

    Create add new plant page

--- wateringServer/app/controllers/plants_controller.rb
+++ wateringServer/app/controllers/plants_controller.rb
@@ -1,9 +1,27 @@
 class PlantsController < ApplicationController
   def new
     if helpers.current_user
+      @plant = Plant.new
       render 'new'
     else
       redirect_to '/login'
     end
+  end
+
+  def create
+    @plant = Plant.new(plant_params)
+
+    if @plant.valid?
+      @plant.save
+      redirect_to helpers.current_user
+    else
+
+    end
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:name, :species, :days_per_watering, :start_date)
   end
 end
