4b70432c1f9ef77042ba958905a98af531c48085
David Armbrust <david.armbrust@gmail.com>
2016-10-12 16:02:11 -0500

    Add before each to build user and add session

--- wateringServer/spec/controllers/plants_controller_spec.rb
+++ wateringServer/spec/controllers/plants_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe PlantsController do
+  before(:each) do
+    User.create(email: "test@test.com", password: "testPassword")
+    controller.session[:user_id] = User.first.id
+  end
 
   describe 'GET #new' do
     it 'can render the :new template' do
