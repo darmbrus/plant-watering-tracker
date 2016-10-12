aa2b49093cb3ad0533720f40713525b8b6730ac5
David Armbrust <david.armbrust@gmail.com>
2016-10-12 18:13:22 -0500

    Add POST test for watering controller

--- wateringServer/spec/controllers/waterings_controller_spec.rb
+++ wateringServer/spec/controllers/waterings_controller_spec.rb
@@ -6,15 +6,23 @@ describe WateringsController do
     controller.session[:user_id] = User.first.id
   end
   let(:plant) { Plant.create(name: "Test Name",
-    species: "Test Species",
-    days_per_watering: 10,
-    start_date: Date.today()
-    )}
+                            species: "Test Species",
+                            days_per_watering: 10,
+                            start_date: Date.today()
+                            ) }
 
   describe 'GET #new' do
     it 'can render the :new template' do
       get :new, params: { plant_id: plant.id }
       expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'POST #create' do
+    it 'can create the new' do
+      post :create, params: { plant_id: plant.id, watering: { amount: 200,
+                                                              date: Date.today()}}
+      expect(Watering.last.amount).to eq 200
     end
   end
 end
