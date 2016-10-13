9a9842fbfbf3d5644b41630c85eab91d5ae09afe
David Armbrust <david.armbrust@gmail.com>
2016-10-12 22:33:32 -0500

    Add tests for post of sessions

--- wateringServer/spec/controllers/sessions_controller_spec.rb
+++ wateringServer/spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe SessionsController do
+  before(:each) do
+    User.create(email: "test@test.com", password: "testPassword")
+  end
+
   describe 'GET #new' do
     it 'can render the login screen' do
       get :new
@@ -8,8 +12,21 @@ describe SessionsController do
     end
 
     it 'can redirect to user page if logged in' do
-      User.create(email: "test@test.com", password: "testPassword")
       controller.session[:user_id] = User.first.id
+      get :new
+      expect(response).to redirect_to("http://test.host/users/#{User.first.id}")
+    end
+  end
+
+  describe 'POST #create' do
+    it 'can assign a session user id' do
+      post :create, params: { session: { email: "test@test.com", password: "testPassword"}}
+      expect(response).to redirect_to("http://test.host/users/#{User.first.id}")
+    end
+
+    it 'can render new if the login fails' do
+      post :create, params: { session: { email: "test@test.com", password: "wrong"}}
+      expect(response).to render_template(:new)
     end
   end
 end
