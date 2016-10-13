b301d8b9860f93a2c1fecd552f8edda4c813c04a
David Armbrust <david.armbrust@gmail.com>
2016-10-13 15:08:26 -0500

    Add persistance of session across requests

--- controller.py
+++ controller.py
@@ -2,6 +2,12 @@ import requests
 
 payload = {'session':{'email':'david.armbrust@gmail.com','password':'Ovation1'}}
 
-r = requests.post('http://localhost:3000/login', json=payload)
+session = requests.Session()
+r = session.post('http://localhost:3000/login', json=payload)
+
+print r.status_code
+
+payload = {'plant':{'name':'Test Name','species':'Test species','days_per_watering':'9','start_date':'9/9/1999'}}
+r = session.post('http://localhost:3000/plants', json=payload)
 
 print r.status_code
