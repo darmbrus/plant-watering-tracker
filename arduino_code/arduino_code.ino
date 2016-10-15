abe7739daa1bcc750f16e6b2725ef167ceceba65
David Armbrust <david.armbrust@gmail.com>
2016-10-15 13:36:32 -0500

    Add physical button press to trigger record build

---
+++ arduino_code/arduino_code.ino
@@ -0,0 +1,18 @@
+const int buttonPin = 2;
+
+int buttonState = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  int randNum = random(300);
+  buttonState = digitalRead(buttonPin);
+  
+  if (buttonState == HIGH) {
+    Serial.println(randNum);
+  }
+  delay(500);
+}
--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 import requests
+import serial
 
 payload = {'session':{'email':'david.armbrust@gmail.com','password':'Ovation1'}}
 
@@ -7,7 +8,15 @@ r = session.post('http://localhost:3000/login', json=payload)
 
 print r.status_code
 
-payload = {'plant':{'name':'Test Name','species':'Test species','days_per_watering':'9','start_date':'9/9/1999'}}
-r = session.post('http://localhost:3000/plants', json=payload)
+# payload = {'plant':{'name':'Test Name','species':'Test species','days_per_watering':'9','start_date':'9/9/1999'}}
+# r = session.post('http://localhost:3000/plants', json=payload)
 
-print r.status_code
+# print r.status_code
+ser = serial.Serial('/dev/cu.usbmodem1421', 9600)
+while True:
+    line_read = ser.readline()
+    print "--------line read---------"
+    print line_read
+    payload = {'plant':{'name':'ArduinoPlant','species':'Test species','days_per_watering':line_read,'start_date':'9/9/1999'}}
+    r = session.post('http://localhost:3000/plants', json=payload)
+ser.close()
